db91b2d7d1a4dacfd64142436180045e235c88e1
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-04-17 10:44:57 -0300

    feat: inital commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config' 
+import { envSchema } from './env'
+import { HttpModule } from './http/http.module'
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    HttpModule,
+  ],
+
+  providers: [],
+})
+export class AppModule {}
