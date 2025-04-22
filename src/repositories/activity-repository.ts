442522f1462cd93e88766113858b9984776bdad3
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-04-22 16:45:03 -0300

    feat: add new methods

--- src/repositories/activity-repository.ts
+++ src/repositories/activity-repository.ts
@@ -1,6 +1,7 @@
-import { ActivityDTO } from "src/@types/activity";
+import { ActivityDTO, ActivityParticipationWithActivity } from "src/@types/activity";
 
 export interface ActivityRepository {
   create(activity: ActivityDTO): Promise<void>
-  
+  fetchAllByInternId(internId: string): Promise<ActivityParticipationWithActivity[]>
+  makeRelationship(internId: string, activityId: string): Promise<void>
 }
