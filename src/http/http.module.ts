5b6dd99d724129738896d27e68c9b196d47f277a
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-04-22 13:39:30 -0300

    chore: confgure module

--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -3,12 +3,14 @@ import { DatabaseModule } from 'src/database/database.module';
 import { CreateInternController } from './controllers/create-intern.controller';
 import { UseCasesModule } from 'src/use-cases/useCases.module';
 import { FetchAllInternsController } from './controllers/fetch-all-interns.controller';
+import { CreateActivityController } from './controllers/create-activity.controller';
 
 
 @Module({
   controllers: [
     CreateInternController,
-    FetchAllInternsController
+    FetchAllInternsController,
+    CreateActivityController
   ],
   providers:[],
   imports: [DatabaseModule, UseCasesModule],
