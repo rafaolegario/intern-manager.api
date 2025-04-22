31572e78d66e4ed73e520094a993ef98c8aae2d0
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-04-22 13:38:42 -0300

    feat: create activity controller

---
+++ src/http/controllers/create-activity.controller.ts
@@ -0,0 +1,32 @@
+import { BadRequestException, Body, ConflictException, Controller, Inject, Post } from '@nestjs/common'
+import { CreateActivityUseCase } from 'src/use-cases/create-activity.service'
+
+
+interface CreateActivityBodySchema {
+  title: string
+  description: string
+  dueDate: Date
+  internIds: string[]
+}
+
+@Controller('/activitys')
+export class CreateActivityController {
+
+  constructor(
+    @Inject('CreateActivityUseCase')
+    private readonly createActivityUseCase: CreateActivityUseCase,
+  ) {}
+
+
+  @Post('/create')
+  async handle(@Body() body: CreateActivityBodySchema) {
+    try {
+      const activity = await this.createActivityUseCase.execute(body)
+      return activity
+    } catch (error) {
+      if(error instanceof BadRequestException){
+      throw new BadRequestException(error.message)
+    }
+  }
+  }
+}
