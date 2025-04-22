0099f69fdeaaa3a862f3fa8e6bcab92a73f057a6
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-04-22 16:43:49 -0300

    feat: add new methods

--- src/database/repositories/prisma-activity-repository.ts
+++ src/database/repositories/prisma-activity-repository.ts
@@ -6,10 +6,31 @@ import { PrismaService } from "../prisma/prisma.service";
 @Injectable()
 export class PrismaActivityRepository implements ActivityRepository {
   constructor(private prisma: PrismaService) {}
+  async fetchAllByInternId(internId: string) {
+    const participations = await this.prisma.activityParticipation.findMany({
+      where: { internId },
+      include: {
+        activity: true,
+      },
+    });
+  
+    return participations
+  }
+
+  async makeRelationship(internId: string, activityId: string){
+    await this.prisma.activityParticipation.create({
+      data: {
+        internId,
+        activityId,
+        score: 0,
+      }
+    });
+  }
+
   async create(activity: ActivityDTO) {
     await this.prisma.activity.create({
       data:activity,
     });
   }
-
+ 
 }
