6a02b9bcb8788878fe8a9416e5c31639b3badd64
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-04-22 16:45:52 -0300

    feat: export new use case

--- src/use-cases/useCases.module.ts
+++ src/use-cases/useCases.module.ts
@@ -1,10 +1,9 @@
-
 import { Module } from '@nestjs/common';
 import { MakeCreateInternUseCase } from './factories/make-create-intern';
 import { DatabaseModule } from 'src/database/database.module';
 import { MakeFetchAllInternsUseCase } from './factories/make-fetch-all-interns';
 import { MakeCreateActivityUseCase } from './factories/make-create-activity';
-
+import { MakeFetchAllInternActivitiesUseCase } from './factories/make-fetch-all-intern-activities';
 
 @Module({
   providers: [
@@ -20,8 +19,12 @@ import { MakeCreateActivityUseCase } from './factories/make-create-activity';
       provide: 'CreateActivityUseCase',
       useFactory: MakeCreateActivityUseCase,
     },
+    {
+      provide: 'FetchAllInternActivitiesUseCase',
+      useFactory: MakeFetchAllInternActivitiesUseCase
+    }
   ],
-  exports: ['CreateInternUseCase', 'FetchAllInternsUseCase', 'CreateActivityUseCase'],
+  exports: ['CreateInternUseCase', 'FetchAllInternsUseCase', 'CreateActivityUseCase', 'FetchAllInternActivitiesUseCase'],
   imports: [DatabaseModule],
 })
 export class UseCasesModule {}
