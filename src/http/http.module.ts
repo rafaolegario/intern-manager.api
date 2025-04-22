860394305115921ee59cebd101297ce15a65ccfc
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-04-22 16:44:18 -0300

    feat: export new controller

--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -4,13 +4,15 @@ import { CreateInternController } from './controllers/create-intern.controller';
 import { UseCasesModule } from 'src/use-cases/useCases.module';
 import { FetchAllInternsController } from './controllers/fetch-all-interns.controller';
 import { CreateActivityController } from './controllers/create-activity.controller';
+import { FetchAllInternActivitiesController } from './controllers/fetch-all-intern-activities.controller';
 
 
 @Module({
   controllers: [
     CreateInternController,
     FetchAllInternsController,
-    CreateActivityController
+    CreateActivityController,
+    FetchAllInternActivitiesController
   ],
   providers:[],
   imports: [DatabaseModule, UseCasesModule],
